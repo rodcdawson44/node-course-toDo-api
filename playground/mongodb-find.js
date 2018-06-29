b8c87c233626e1d61ef236203be1999a429b6c96
R.C.  Dawson <r.c.dawson@RCs-MacBook-Pro.local>
2018-06-29 00:23:02 -0700

    Init Commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient,ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to Mongo DB server');
+const db= client.db('TodoApp');
+
+// db.collection('Todos').find({completed:false}).toArray().then((docs) => {
+//   console.log('Todos');
+//   console.log(JSON.stringify(docs,undefined,2));
+// }, (err) => {
+//    console.log('Unable to fetch todos', err);
+// });
+
+// db.collection('Todos').find().count().then((count) => {
+//   console.log(`Todos count ${count}`);
+// }, (err)  =>{
+//   console.log('Unable to fetch todos',err);
+// });
+
+db.collection('Users').find({name: 'RC'}).toArray().then((docs) => {
+  console.log(JSON.stringify(docs,undefined,2));
+});
+
+//  client.close();
+} );
