9842272ac99839dd02b9609ae432675c19a6b5ec
R.C.  Dawson <r.c.dawson@RCs-MacBook-Pro.local>
2018-06-29 21:41:04 -0700

    'delete commit'

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,20 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient,ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to Mongo DB server');
+const db= client.db('TodoApp');
+
+db.collection('Todos').deleteMany({text: 'eat lunch'}).then((result) => {
+  console.log(result);
+});
+
+//  client.close();
+} );
