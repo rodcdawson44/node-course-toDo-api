78c7cab6b8c87406da7485c073ee5ae27b2c2a54
R.C.  Dawson <r.c.dawson@RCs-MacBook-Pro.local>
2018-07-08 20:57:13 -0700

    Setup app for Heroku

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ var {Todo} = require ('./models/todo');
 var {User} = require ('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
+
+
 
 app.use(bodyParser.json());
 
@@ -52,8 +55,8 @@ if(!ObjectID.isValid(id)){
 
 
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`started up at port ${port}`);
 });
 
 module.exports = {app};
--- server/tests/server.tests.js
+++ server/tests/server.tests.js
@@ -1,14 +1,19 @@
 const expect = require('expect');
 const request= require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First todo'
 },{
+  _id: new ObjectID(),
   text:'second todo'
-},{ text: 'third todo'
+},{
+  id: new ObjectID(),
+   text: 'third todo'
 }];
 
 beforeEach((done) =>{
@@ -68,5 +73,42 @@ describe('Get/todos', ()=>{
        expect(res.body.todos.length).toBe(3);
      })
      .end(done);
+  });
+});
+
+describe('GET/todos/:id', () =>{
+  it('should return todo doc', (done) =>{
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        console.log(res.body)
+        expect(res.body.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+
+  it('should return 404 if todo not found', (done) =>{
+      var ident = new ObjectID();
+    request(app)
+      .get(`/todos/${ident.toHexString()}`)
+      .expect(404)
+      .expect((res) => {
+        console.log(`The status is ${res.status}` )
+        expect(res.status).toBe(404)
+      })
+   .end(done);
+  });
+
+  it('should return 404 if id not valid', (done) =>{
+    request(app)
+      .get('/todos/123')
+      .expect(404)
+      .expect((res) => {
+        console.log(`The status is ${res.status}`)
+        expect(res.status).toBe(404)
+      })
+      .end(done);
   });
 });
