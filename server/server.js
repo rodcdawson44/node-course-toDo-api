ae523861f38b0189460d46a4e05614a18f282ddb
R.C.  Dawson <r.c.dawson@RCs-MacBook-Pro.local>
2018-07-11 22:25:32 -0700

    Testing delete and fixing delete errors

--- server/server.js
+++ server/server.js
@@ -53,6 +53,23 @@ if(!ObjectID.isValid(id)){
 });
 
 
+app.delete('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) =>{
+    if(!todo){
+      return res.status(404).send();
+    }
+  res.send(todo);
+}).catch((e) =>{
+  res.status(404).send();
+});
+});
+
+
 
 
 app.listen(port, () => {
