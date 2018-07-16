ba5dd9c62bab4eb5dd252441f2043b9cf1638f2f
R.C.  Dawson <r.c.dawson@RCs-MacBook-Pro.local>
2018-07-15 21:05:23 -0700

    hashing passwords

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 // {
 //   email: 'rc123@gmail.com'
 //   password: 'abcdefghijk'
@@ -74,6 +75,23 @@ try {
 
   });
 };
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  if(user.isModified('password')){
+    bcrypt.genSalt(10, (err,salt) =>{
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else{
+    next();
+  }
+});
+
 var User = mongoose.model('User', UserSchema);
+
 
 module.exports = { User }
